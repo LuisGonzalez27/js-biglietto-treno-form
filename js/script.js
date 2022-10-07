b1b0d91ee9c2a48a448c6ed59cacf556fd25d9a0
LuisGonzalez27 <luis27coding@gmail.com>
2022-10-07 16:06:10 +0200

    MILESTONE 1

---
+++ js/script.js
@@ -0,0 +1,50 @@
+"user strict";
+
+const btn = document.getElementById('calcola');
+
+const calcolaBiglietto = function(){
+    let km = document.getElementById("km").value;
+    console.log("Numero di chilometri da percorrere :", km);
+
+    let age = document.getElementById("age").value;
+    console.log("Età del passeggero :", age);
+
+    const prezzoKm = 0.21;
+    const scontoUnder = 0.20;
+    const prezzoOver = 0.40;
+    let prezzoBiglietto = km * prezzoKm;
+    console.log("Prezzo del biglietto :", prezzoBiglietto);
+ 
+    if(age <= 18){
+        prezzoBiglietto -= prezzoBiglietto * scontoUnder;
+        console.log('Il prezzo con lo sconto under 18 è di : ' + prezzoBiglietto);
+
+        let centesimi = prezzoBiglietto;
+        centesimi = centesimi.toFixed(2);
+        console.log('Il prezzo con i decimali : ' + centesimi);
+    
+        const prezzoStampato = document.getElementById('prezzoFinale');
+        prezzoStampato.innerHTML = centesimi + " €";
+    } 
+    else if(age >= 65){
+        prezzoBiglietto -= prezzoBiglietto * prezzoOver;
+        console.log('Il prezzo con lo sconto over 65 è di : ' + prezzoBiglietto);
+
+        let centesimi = prezzoBiglietto;
+        centesimi = centesimi.toFixed(2);
+        console.log('Il prezzo con i decimali : ' + centesimi);
+    
+        const prezzoStampato = document.getElementById('prezzoFinale');
+        prezzoStampato.innerHTML = centesimi + " €";
+    } 
+    else{
+        let centesimi = prezzoBiglietto;
+        centesimi = centesimi.toFixed(2);
+        console.log('Il prezzo in decimali senza sconto età è di : ' + centesimi);
+    
+        const prezzoStampato = document.getElementById('prezzoFinale');
+        prezzoStampato.innerHTML = centesimi + " €";
+    } 
+}
+
+btn.addEventListener('click', calcolaBiglietto);
